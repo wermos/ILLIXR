96840afe8ba8c93f6bba616420a103cd1ef46017
wermos <63574588+wermos@users.noreply.github.com>
2023-04-13 19:39:29 +0530

    Converted the ternary operator into a more readable if-else string.

--- common/global_module_defs.hpp
+++ common/global_module_defs.hpp
@@ -62,12 +62,16 @@ struct rendering_params {
 };
 
 /**
- * @brief Convert a string containing a (python) boolean to the bool type
+ * @brief Convert a string containing a (Python) boolean to the bool type
  */
 inline bool str_to_bool(std::string var) {
-    return (var == "True") ? true
-        : (var == "False") ? false
-                           : throw std::runtime_error("Invalid conversion from std::string to bool");
+    if (var == "True") {
+        return true;
+    } else if (var == "False") {
+        return false;
+    } else {
+        throw std::runtime_error("Invalid conversion from std::string to bool");
+    }
 }
 
 /// Temporary environment variable getter. Not needed once #198 is merged.
